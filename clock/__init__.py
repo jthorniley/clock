47453906d4c589c564459cf47b4e2a576d48c258
James Thorniley <james.thorniley@gmail.com>
2019-11-11 07:04:46 +0000

    Plotting code for a simple pendulum model

--- clock/__init__.py
+++ clock/__init__.py
@@ -1,1 +1,19 @@
 __version__ = '0.1.0'
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import solve_ivp
+
+
+def pendulum(t, y):
+    """Simple pendulum model."""
+
+    return np.array([-y[1], y[0]])
+
+
+
+def solve_and_plot_pendulum():
+    sol = solve_ivp(pendulum, (0, 10), [0, 1])
+
+    plt.plot(sol.t, sol.y.T)
+    plt.savefig('image.png')
